1663c0424568aaab363e8cc0d1e8bf29a9c5a7c2
Abs Shagor <92050253+Abs-Shagor@users.noreply.github.com>
2024-03-17 21:47:37 +0600

    Create Formula.cpp

---
+++ Math x Combinatorics.cpp/Formula.cpp
@@ -0,0 +1,19 @@
+/*
+
+1.sum of all number between 1 to n = (n*(n+1))/2
+
+2. sum of all even number  between 1 to n = (n*(n+2))/4   [ here, n is even ]
+
+3. sum of all odd number between 1 to n = (n/2)^2    [ here, n is even ] 
+
+4. sum of 1^2 + 2^2 + 3^2 + 4^2 + …..+ n^2 = (n*(n+1))/2 *  ((2n+1))/2
+
+5. if the difference of 2 consecutive number of a series is same ,then		Here,
+   The nth term of the series ,  T(n) = a+(n-1)*d				a = first term
+   The sum of the 1st nth term , S(n) = (n/2)*(a+l) or (n/2)*(2a+(n-1)d)	d = difference
+
+6. if the ratio of 2 consecutive number of a series is same ,then		r = ratio(2nd/1st)
+   The nth term of the series , T(n) = a*r^(n-1)				l = last term
+   The sum of the 1st nth term, S(n) = (a*(r^n-1)/(r-1)
+   
+*/
