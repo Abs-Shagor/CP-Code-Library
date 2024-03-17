9ada007521890d275b72c5138a71192f863e0372
Abs Shagor <92050253+Abs-Shagor@users.noreply.github.com>
2024-03-17 22:10:35 +0600

    Create Formula.cpp

---
+++ Geometry/Formula.cpp
@@ -0,0 +1,21 @@
+//////////	 == > Geometry Formula ? < ==
+Area of a Triangle = ½ × base × height
+                   = √s(s−a)(s−b)(s−c) // s = half perimeter
+                   = 1/2 × bc × sin(A) // A = angle between b and c
+                   = 1/2 × ac × sin(B)
+                   = (√3)/4 × side2 // area of equilateral triangle
+                   = 1/4 × b√4a2−b2 // area of isosceles triangle
+                   = (b*h)/2 ( side: a,a,b ) // area of isosceles triangle
+Area of  Rectangle = a*b (side : a,b)
+                 d = √(a2 + b2) (diagonal length of Rectangle) 
+Area of  Trapizium = ½(a + b)h
+Area of a Cylinder = 2πrh
+Area of a Cylinder = 2πr(r + h) (Total)
+Area of  a  Sphere = 4πr2
+Area  of   a  cone =  πrl
+Total surface area of pyramid = b^2 + 2bs , here b = base and s = slant height
+Area of  a  Circle = π×r2 
+                   = 2πr (Circumference
+For (x1,x2)&(y1,y2)= √(x2-x1)^2 + (y2-y2)^2 (distance)
+                   = ((x1+x2)/2, (y1+y2)/2) (Midpoint)
+                   = ((y2-y1)/x2-x1) (slop) 	
